94a8e8ed43f770a21d4cf1d8e0d24b343fee0496
Phan Huy Hoang <hoangph271@gmail.com>
2023-04-13 01:42:53 +0700

    [Fix] Wrong filename

--- restart-docker-compose.ps1
+++ restart-docker-compose.ps1
@@ -2,4 +2,4 @@ docker compose build;
 docker compose restart;
 
 # To execute this script:
-# powershell.exe -ExecutionPolicy Bypass -File .\restart-docker-compose.ps
+# powershell.exe -ExecutionPolicy Bypass -File .\restart-docker-compose.ps1
